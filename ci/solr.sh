6cb259dfaef22d824d9d4b9e567f1e686c5925c5
Edwin Cruz <softr8@gmail.com>
2013-04-03 12:20:55 -0600

    starting solr using sunspot-solr bash script

---
+++ ci/solr.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set +e
+
+solr_responding() {
+  port=$1
+  curl -o /dev/null "http://localhost:$port/solr/admin/ping" > /dev/null 2>&1
+}
+
+wait_until_solr_responds() {
+  port=$1
+  i=0
+  while ! solr_responding $1 ; do
+    /bin/echo -n "$i."
+    sleep 1
+    i=$((i+1))
+    if [ $i -eq 15 ]
+    then
+      /bin/echo -n "Solr didn't start before 15 seconds"
+      break
+    fi
+  done
+}
+
+
+/bin/echo -n "Starting Solr on port 8983 for specs..."
+if [ -f sunspot-solr.pid ]; then bundle exec sunspot-solr stop || true; fi
+
+sunspot-solr start -p 8983 -d /tmp/solr
+wait_until_solr_responds 8983
+/bin/echo "done."
